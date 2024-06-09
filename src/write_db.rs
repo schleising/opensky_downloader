d8cdca43d34c404f4262d89dee011d1580581a79
schleising <stephen@schleising.net>
2024-06-09 22:18:13 +0100

    Switch to live database and create an index on tail number

--- src/write_db.rs
+++ src/write_db.rs
@@ -1,18 +1,21 @@
 use std::error::Error;
 
 use mongodb::sync::Client;
+use mongodb::IndexModel;
+
+use bson::doc;
 
 use crate::models::Aircraft;
 
 pub fn write_to_db(aircraft_vec: Vec<Aircraft>) -> Result<(), Box<dyn Error>> {
     // Create a MongoDB client
-    let client: Client = Client::with_uri_str("mongodb://localhost:27017")?;
+    let client: Client = Client::with_uri_str("mongodb://macmini2:27017")?;
 
     // Get the database
-    let db: mongodb::sync::Database = client.database("opensky");
+    let db: mongodb::sync::Database = client.database("web_database");
 
     // Get the collection
-    let collection: mongodb::sync::Collection<Aircraft> = db.collection("aircraft");
+    let collection: mongodb::sync::Collection<Aircraft> = db.collection("aircraft_collection");
 
     // Drop the collection if it already exists
     println!("Dropping the collection...");
@@ -21,6 +24,11 @@ pub fn write_to_db(aircraft_vec: Vec<Aircraft>) -> Result<(), Box<dyn Error>> {
     // Insert the aircraft into the collection
     println!("Inserting {} documents into the database...", aircraft_vec.len());
     collection.insert_many(aircraft_vec, None)?;
+
+    // Create an index on the registration field
+    println!("Creating an index on the registration field...");
+    let index = IndexModel::builder().keys(doc! { "registration": 1 }).build();
+    collection.create_index(index, None)?;
 
     // Return Ok
     return Ok(());
