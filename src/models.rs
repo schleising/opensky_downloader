624dc0e9d3f312f0d532568365f6ebd4d0743598
schleising <stephen@schleising.net>
2024-06-22 14:52:27 +0100

    No need for Debug or Clone traints on Aircraft struct

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Aircraft {
     pub icao24: String,
     pub registration: String,
